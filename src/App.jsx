cc296e9ceb641aa9b0ce236263ebeba11bdbeab0
PasinPeat <pasin.lawn1995@gmail.com>
2024-01-18 15:13:27 +0700

    fix:rename state input and result

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,43 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [searchText, setSearchText] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [result, setResult] = useState([]);
 
-  const searchFromServer = async (text) => {
+  const fetchingData = async (text) => {
     try {
       const response = await axios.get(
         `https://www.googleapis.com/books/v1/volumes?q=${text}`
       );
-      setSearchResult(response.data.items);
+      setResult(response.data.items);
       console.log(response.data.items);
     } catch (error) {
       console.log(error);
     }
   };
-  //Case_1. with useEffect
-useEffect(() => {
-    if (searchText) {
-      searchFromServer(searchText);
-    }
-  }, [searchText]);
+  // Case_1. without useEffect
+    // if (inputText) {
+    //   fetchingData(inputText);
+    // }
 
-//Case_2. without useEffect
-  // if (searchText) {
-  //   searchFromServer(searchText);
-  // }
+
+  //Case_2. with useEffect
+useEffect(() => {
+    if (inputText) {
+      fetchingData(inputText);
+    }
+  },[inputText]);
+
+
   return (
     <div className="App">
       <h1>Find a Book</h1>
-      <input type="text" onChange={(e) => setSearchText(e.target.value)} />
-      {searchResult.length > 0 && (
+      <input type="text" onChange={(e) => setInputText(e.target.value)} />
+      {result.length > 0 && (
         <>
-          {searchResult.map((result, index) => (
+          {result.map((list, index) => (
             <div key={index} className="autocompleteItems">
-              <li className="search-result">{result.volumeInfo.title}</li>
+              <li className="search-result">{list.volumeInfo.title}</li>
             </div>
           ))}
         </>
